324dcc7b9d01424c69d198b296637bea5f5375c9
Nicolò Curioni <nicolo.curioni96@gmail.com>
2023-10-08 16:20:41 +0200

    Created `FeedItem` model

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Nicolò Curioni  on 08/10/23.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
