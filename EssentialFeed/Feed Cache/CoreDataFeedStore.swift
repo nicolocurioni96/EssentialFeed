238b337413b70b0908d2a6407d369a6aaa144859
Nicolò Curioni <nicolo.curioni96@gmail.com>
2024-01-25 15:44:00 +0100

    Extract duplicate code into a reusable helper method

--- EssentialFeed/Feed Cache/CoreDataFeedStore.swift
+++ EssentialFeed/Feed Cache/CoreDataFeedStore.swift
@@ -18,7 +18,8 @@ public final class CoreDataFeedStore: FeedStore {
     
     public func retrieve(completion: @escaping RetrievalCompletion) {
         let context = self.context
-        context.perform {
+        
+        perform { context in
             do {
                 if let cache = try ManagedCache.find(in: context) {
                     completion(.found(feed: cache.localFeed, timestamp: cache.timestamp))
@@ -33,7 +34,8 @@ public final class CoreDataFeedStore: FeedStore {
     
     public func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
         let context = self.context
-        context.perform {
+        
+        perform { context in
             do {
                 let managedCache = try ManagedCache.newUniqueInstance(in: context)
                 managedCache.timestamp = timestamp
@@ -50,7 +52,7 @@ public final class CoreDataFeedStore: FeedStore {
     public func deleteCachedFeed(completion: @escaping DeletionCompletion) {
         let context = self.context
         
-        context.perform {
+        perform { context in
             do {
                 try ManagedCache.find(in: context).map(context.delete).map(context.save)
                 completion(nil)
@@ -58,6 +60,12 @@ public final class CoreDataFeedStore: FeedStore {
                 completion(error)
             }
         }
+    }
+    
+    private func perform(_ action: @escaping (NSManagedObjectContext) -> Void) {
+        let context = self.context
+        
+        context.perform { action(context) }
     }
 }
 
