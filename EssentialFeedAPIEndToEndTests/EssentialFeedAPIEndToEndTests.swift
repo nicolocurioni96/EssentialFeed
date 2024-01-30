e6e249a1c9927b2832eafd2835c1f9b747560eb6
Nicolò Curioni <nicolo.curioni96@gmail.com>
2024-01-30 16:14:19 +0100

    Improved code on `EssentialFeedAPIEndToEndTests` in order to make test passing and included `EssentialFeedCacheIntegrationTests` test target in the `CI` scheme to guarantee we build and run all cache integration tests as part of the continous integration pipeline

--- EssentialFeedAPIEndToEndTests/EssentialFeedAPIEndToEndTests.swift
+++ EssentialFeedAPIEndToEndTests/EssentialFeedAPIEndToEndTests.swift
@@ -30,30 +30,48 @@ final class EssentialFeedAPIEndToEndTests: XCTestCase {
     }
     
     // MARK: - Helper methods
+    private func getFeedResult(file: StaticString = #file, line: UInt = #line) -> LoadFeedResult? {
+        let testServerURL = URL(string: "https://essentialdeveloper.com/feed-case-study/test-api/feed")!
+        let client = URLSessionHTTPClient(session: URLSession(configuration: .ephemeral))
+        let loader = RemoteFeedLoader(client: client, url: testServerURL)
+        trackForMemoryLeaks(client, file: file, line: line)
+        trackForMemoryLeaks(loader, file: file, line: line)
+        
+        let exp = expectation(description: "Wait for load completion")
+        
+        var receivedResult: LoadFeedResult?
+        loader.load { result in
+            receivedResult = result
+            exp.fulfill()
+        }
+        wait(for: [exp], timeout: 5.0)
+        
+        return receivedResult
+    }
+    
     private func expectedImage(at index: Int) -> FeedImage {
-        FeedImage(
-            id: expectedID(at: index),
-            description: expectedDescription(at: index),
-            location: expectedLocation(at: index),
+        return FeedImage(
+            id: id(at: index),
+            description: description(at: index),
+            location: location(at: index),
             url: imageURL(at: index))
     }
     
-    private func expectedID(at index: Int) -> UUID {
-        UUID(
-            uuidString: [
-                "853DF947-A034-491B-BC0F-8E0E224A49E2",
-                "BA473803-875A-4FB8-852F-A53D2BADC8BC",
-                "93EED266-A585-4A38-A7C8-9035CEF0AF00",
-                "094223AF-84DE-424D-809C-D2CCD2969BDD",
-                "74C3E1B6-3999-4CCC-9472-00E7F68ECFA5",
-                "1C2FFA4F-A465-4643-8B30-282A3B991F86",
-                "6A629552-73A9-4E58-8185-FA2A45C37060",
-                "29F4997F-2A53-4B9A-A705-416C6DD572AF",
-            ][index])!
+    private func id(at index: Int) -> UUID {
+        return UUID(uuidString: [
+            "73A7F70C-75DA-4C2E-B5A3-EED40DC53AA6",
+            "BA298A85-6275-48D3-8315-9C8F7C1CD109",
+            "5A0D45B3-8E26-4385-8C5D-213E160A5E3C",
+            "FF0ECFE2-2879-403F-8DBE-A83B4010B340",
+            "DC97EF5E-2CC9-4905-A8AD-3C351C311001",
+            "557D87F1-25D3-4D77-82E9-364B2ED9CB30",
+            "A83284EF-C2DF-415D-AB73-2A9B8B04950B",
+            "F79BD7F8-063F-46E2-8147-A67635C3BB01"
+        ][index])!
     }
     
-    private func expectedDescription(at index: Int) -> String? {
-        [
+    private func description(at index: Int) -> String? {
+        return [
             "Description 1",
             nil,
             "Description 3",
@@ -61,12 +79,12 @@ final class EssentialFeedAPIEndToEndTests: XCTestCase {
             "Description 5",
             "Description 6",
             "Description 7",
-            "Description 8",
+            "Description 8"
         ][index]
     }
     
-    private func expectedLocation(at index: Int) -> String? {
-        [
+    private func location(at index: Int) -> String? {
+        return [
             "Location 1",
             "Location 2",
             nil,
@@ -74,32 +92,11 @@ final class EssentialFeedAPIEndToEndTests: XCTestCase {
             "Location 5",
             "Location 6",
             "Location 7",
-            "Location 8",
+            "Location 8"
         ][index]
     }
     
     private func imageURL(at index: Int) -> URL {
         return URL(string: "https://url-\(index+1).com")!
-    }
-    
-    private func getFeedResult(file: StaticString = #file, line: UInt = #line) -> LoadFeedResult? {
-        let url = URL(string: "https://my-json-server.typicode.com/nicolocurioni96/EssentialFeed/response")!
-        let client = URLSessionHTTPClient(session: URLSession(configuration: .ephemeral))
-        let loader = RemoteFeedLoader(client: client, url: url)
-        let expectation = expectation(description: "Wait for load completion")
-        
-        trackForMemoryLeaks(client, file: file, line: line)
-        trackForMemoryLeaks(loader, file: file, line: line)
-        
-        var receivedResult: LoadFeedResult?
-        
-        loader.load { result in
-            receivedResult = result
-            expectation.fulfill()
-        }
-        
-        wait(for: [expectation], timeout: 5.0)
-        
-        return receivedResult
     }
 }
