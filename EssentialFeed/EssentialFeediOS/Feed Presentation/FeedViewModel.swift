aa63a9e2389327a074fdf915fd11a7b4daea640a
Nicolò Curioni <nicolo.curioni96@gmail.com>
2024-02-16 14:28:51 +0100

    Move ViewModels on separate files

---
+++ EssentialFeed/EssentialFeediOS/Feed Presentation/FeedViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  FeedViewModel.swift
+//  EssentialFeediOS
+//
+//  Created by Nicolò Curioni  on 16/02/24.
+//
+
+import Foundation
+import EssentialFeed
+
+struct FeedViewModel {
+    let feed: [FeedImage]
+}
