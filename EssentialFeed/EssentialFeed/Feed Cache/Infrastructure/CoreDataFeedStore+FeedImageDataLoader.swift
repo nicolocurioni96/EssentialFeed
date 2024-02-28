6eeffd5d9f07993044e140c2f242ad8b57ca3efd
Nicolò Curioni <nicolo.curioni96@gmail.com>
2024-02-28 19:28:25 +0100

    Move `CoreDataFeedStore` implementation of the `FeedImageDataLoader` protocol to production

---
+++ EssentialFeed/EssentialFeed/Feed Cache/Infrastructure/CoreDataFeedStore+FeedImageDataLoader.swift
@@ -0,0 +1,18 @@
+//
+//  CoreDataFeedStore.swift
+//  EssentialFeed
+//
+//  Created by Nicolò Curioni  on 28/02/24.
+//
+
+import Foundation
+
+extension CoreDataFeedStore: FeedImageDataStore {
+    public func insert(_ data: Data, for url: URL, completion: @escaping (FeedImageDataStore.InsertionResult) -> Void) {
+        
+    }
+    
+    public func retrieve(dataForURL url: URL, completion: @escaping (FeedImageDataStore.RetrievalResult) -> Void) {
+        completion(.success(.none))
+    }
+}
