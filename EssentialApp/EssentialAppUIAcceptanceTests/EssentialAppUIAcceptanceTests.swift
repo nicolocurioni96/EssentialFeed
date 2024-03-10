73df88adaafea631f6cea2e1f5503b935ea01f96
Nicolò Curioni <nicolo.curioni96@gmail.com>
2024-03-10 18:29:00 +0100

    Add UI Test target for running high-level acceptance tests

---
+++ EssentialApp/EssentialAppUIAcceptanceTests/EssentialAppUIAcceptanceTests.swift
@@ -0,0 +1,12 @@
+//
+//  EssentialAppUIAcceptanceTests.swift
+//  EssentialAppUIAcceptanceTests
+//
+//  Created by Nicolò Curioni  on 10/03/24.
+//
+
+import XCTest
+
+final class EssentialAppUIAcceptanceTests: XCTestCase {
+
+}
