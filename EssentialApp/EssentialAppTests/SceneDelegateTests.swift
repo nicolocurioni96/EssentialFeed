5e8a1d40749361571f3cc70b23550c81a9175c76
Nicolò Curioni <nicolo.curioni96@gmail.com>
2024-04-27 16:16:06 +0200

    Make private `UIWindowSpy` class with `makeKeyAndVisibleCallCount` variable by incrementing the count to one in the inherited `makeKeyAndVisible` method

--- EssentialApp/EssentialAppTests/SceneDelegateTests.swift
+++ EssentialApp/EssentialAppTests/SceneDelegateTests.swift
@@ -8,14 +8,13 @@ import EssentialFeediOS
 
 class SceneDelegateTests: XCTestCase {
     func test_configureWindow_setsWindowAsKeyAndVisible() {
-        let window = UIWindow()
+        let window = UIWindowSpy()
         let sut = SceneDelegate()
         sut.window = window
         
         sut.configureWindow()
         
-        XCTAssertFalse(window.isKeyWindow, "Expected window to be the key window")
-        XCTAssertFalse(window.isHidden, "Expected window to be visible")
+        XCTAssertEqual(window.makeKeyAndVisibleCallCount, 1, "Expected to make key and visible")
     }
     
     func test_configureWindow_configuresRootViewController() {
@@ -30,5 +29,14 @@ class SceneDelegateTests: XCTestCase {
         
         XCTAssertNotNil(rootNavigation, "Expected a navigation controller as root, got \(String(describing: root)) instead")
         XCTAssertTrue(topController is FeedViewController, "Expected a feed controller as top view controller, got \(String(describing: topController)) instead")
+    }
+    
+    // MARK: - Helpers
+    private class UIWindowSpy: UIWindow {
+        var makeKeyAndVisibleCallCount = 0
+        
+        override func makeKeyAndVisible() {
+            makeKeyAndVisibleCallCount += 1
+        }
     }
 }
