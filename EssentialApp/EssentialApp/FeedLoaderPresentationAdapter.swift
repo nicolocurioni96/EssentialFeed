6552fdc08c2d28e8e7f61a0ebba4f9d0fcbf0c72
Nicolò Curioni <nicolo.curioni96@gmail.com>
2024-05-12 23:01:40 +0200

    Make Presentation Adapter generic so it can be reused

--- EssentialApp/EssentialApp/FeedLoaderPresentationAdapter.swift
+++ EssentialApp/EssentialApp/FeedLoaderPresentationAdapter.swift
@@ -9,19 +9,19 @@ import Combine
 import EssentialFeed
 import EssentialFeediOS
 
-final class FeedLoaderPresentationAdapter: FeedViewControllerDelegate {
-    private let feedLoader: () -> AnyPublisher<[FeedImage], Error>
+final class LoadResourcePresentationAdapter<Resource, View: ResourceView> {
+    private let loader: () -> AnyPublisher<Resource, Error>
     private var cancellable: Cancellable?
-    var presenter: LoadResourcePresenter<[FeedImage], FeedViewAdapter>?
+    var presenter: LoadResourcePresenter<Resource, View>?
     
-    init(feedLoader: @escaping () -> AnyPublisher<[FeedImage], Error>) {
-        self.feedLoader = feedLoader
+    init(loader: @escaping () -> AnyPublisher<Resource, Error>) {
+        self.loader = loader
     }
     
-    func didRequestFeedRefresh() {
+    func loadResource() {
         presenter?.didStartLoading()
         
-        cancellable = feedLoader()
+        cancellable = loader()
             .dispatchOnMainQueue()
             .sink { [weak self] completion in
                 switch completion {
@@ -29,8 +29,14 @@ final class FeedLoaderPresentationAdapter: FeedViewControllerDelegate {
                 case let .failure(error):
                     self?.presenter?.didFinishLoading(with: error)
                 }
-            } receiveValue: { [weak self] feed in
-                self?.presenter?.didFinishLoading(with: feed)
+            } receiveValue: { [weak self] resource in
+                self?.presenter?.didFinishLoading(with: resource)
             }
+    }
+}
+
+extension LoadResourcePresentationAdapter: FeedViewControllerDelegate {
+    func didRequestFeedRefresh() {
+        loadResource()
     }
 }
