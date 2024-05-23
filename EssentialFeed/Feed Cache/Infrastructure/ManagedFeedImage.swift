6960f6bcaefd38dedc065be7fd25a60da3cf1af0
Nicolò Curioni <nicolo.curioni96@gmail.com>
2024-05-23 14:26:43 +0200

    Optimize CoreData implementation to reduce cache misses

--- EssentialFeed/Feed Cache/Infrastructure/ManagedFeedImage.swift
+++ EssentialFeed/Feed Cache/Infrastructure/ManagedFeedImage.swift
@@ -27,21 +27,33 @@ extension ManagedFeedImage {
     }
     
     static func images(from localFeed: [LocalFeedImage], in context: NSManagedObjectContext) -> NSOrderedSet {
-        return NSOrderedSet(array: localFeed.map { local in
+        let images =  NSOrderedSet(array: localFeed.map { local in
             let managed = ManagedFeedImage(context: context)
             managed.id = local.id
             managed.imageDescription = local.description
             managed.location = local.location
             managed.url = local.url
+            managed.data = context.userInfo[local.url] as? Data
             return managed
         })
+        
+        context.userInfo.removeAllObjects()
+        return images
     }
     
     static func data(with url: URL, in context: NSManagedObjectContext) throws -> Data? {
+        if let data = context.userInfo[url] as? Data { return data }
+        
         return try first(with: url, in: context)?.data
     }
     
     var local: LocalFeedImage {
         return LocalFeedImage(id: id, description: imageDescription, location: location, url: url)
+    }
+    
+    override func prepareForDeletion() {
+        super.prepareForDeletion()
+        
+        managedObjectContext?.userInfo[url] = data
     }
 }
