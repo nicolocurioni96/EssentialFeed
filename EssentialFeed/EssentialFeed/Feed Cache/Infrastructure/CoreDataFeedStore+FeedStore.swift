c923cf332d16a85dcb2f3f5939f4de91aa8e4d29
Nicolò Curioni <nicolo.curioni96@gmail.com>
2024-05-26 10:53:18 +0200

    Rename method to clarify intent with `performAsync`

--- EssentialFeed/EssentialFeed/Feed Cache/Infrastructure/CoreDataFeedStore+FeedStore.swift
+++ EssentialFeed/EssentialFeed/Feed Cache/Infrastructure/CoreDataFeedStore+FeedStore.swift
@@ -10,7 +10,7 @@ import CoreData
 extension CoreDataFeedStore: FeedStore {
     
     public func retrieve(completion: @escaping RetrievalCompletion) {
-        perform { context in
+        performAsync { context in
             completion(Result {
                 try ManagedCache.find(in: context).map {
                     CachedFeed(feed: $0.localFeed, timestamp: $0.timestamp)
@@ -20,7 +20,7 @@ extension CoreDataFeedStore: FeedStore {
     }
     
     public func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
-        perform { context in
+        performAsync { context in
             completion(Result {
                 let managedCache = try ManagedCache.newUniqueInstance(in: context)
                 managedCache.timestamp = timestamp
@@ -31,7 +31,7 @@ extension CoreDataFeedStore: FeedStore {
     }
     
     public func deleteCachedFeed(completion: @escaping DeletionCompletion) {
-        perform { context in
+        performAsync { context in
             completion(Result {
                 try ManagedCache.deleteCache(in: context)
             })
