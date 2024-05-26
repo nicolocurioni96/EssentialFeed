9e746c3d63eb7a01439475d459ddf3bf4983b0fe
Nicolò Curioni <nicolo.curioni96@gmail.com>
2024-05-26 21:11:16 +0200

    Delete NullStore and replaced with `InMemoryFeedStore`

--- EssentialApp/EssentialApp/SceneDelegate.swift
+++ EssentialApp/EssentialApp/SceneDelegate.swift
@@ -43,7 +43,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         } catch {
             assertionFailure("Failed to instantiate CoreData store with error: \(error.localizedDescription)")
             logger.fault("Failed to instantiate CoreData store with error: \(error.localizedDescription)")
-            return NullStore()
+            return InMemoryFeedStore()
         }
     }()
     
